839de4ddf8a82e475b15d686b743887f144934fc
suhaila <suhaila.kp@zoft.care>
2025-05-23 15:06:24 +0530

    fix: clean up next.config.ts to avoid redeclaration

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
 };
 
 export default nextConfig;
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-}
-
-module.exports = nextConfig
 
